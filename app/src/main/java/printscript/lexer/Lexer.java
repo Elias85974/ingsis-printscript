93c00034ab2927f9fbdb2604bfbe022f0cdc25e4
JuliIglesias <jiglesias@mail.austral.edu.ar>
2024-08-02 15:44:05 -0300

    lexer falta column

--- app/src/main/java/printscript/lexer/Lexer.java
+++ app/src/main/java/printscript/lexer/Lexer.java
@@ -1,7 +1,6 @@
 package printscript.lexer;
 
 import printscript.Token;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +23,8 @@ public class Lexer {
 
         List<Token> tokens = new ArrayList<>();
 
-        Character stringInitializer = null;
+        int spaceCounter = 0;
+        Character stringInitializer = null; //Guarda la comilla inicial
 
         for (int charIndex = 0; charIndex < charArrayLength; charIndex++) {
             char currentChar = charArray[charIndex];
@@ -37,10 +37,11 @@ public class Lexer {
                 currentWord += currentChar;
                 for (int charIndexS = charIndex + 1; charIndexS < charArrayLength; charIndexS++) {
                     char currentCharS = charArray[charIndexS];
-                    currentWord += currentCharS;
+                    currentWord += currentCharS; //agrega hasta coincidir el char con el strInit
 
                     if (currentCharS == stringInitializer) {
-                        tokens.add(new Token(identifier, currentWord, row, column)); // toDo resolver tema del column y row
+                        // Si el caracter actual es igual que la comilla inicial, se termino el string
+                        tokens.add(new Token(identifier, currentWord, row, column)); // toDo resolver tema del column
                         currentWord = "";
                         charIndex = charIndexS;
                         break;
@@ -55,13 +56,13 @@ public class Lexer {
 
                 for (int charIndexN = charIndex + 1; charIndexN < charArrayLength; charIndexN++) {
                     char currentCharN = charArray[charIndexN];
-
+                    // apendea el numero o el . al currentWord y si no es lo agrega como toen
                     if (Character.isDigit(currentCharN) || currentCharN == '.') {
                         currentWord += currentCharN;
                     } else {
-                        tokens.add(new Token(identifier, currentWord, row, column)); // toDo resolver tema del column y row
+                        tokens.add(new Token(identifier, currentWord, row, column)); // toDo resolver tema del column
                         currentWord = "";
-                        charIndex = charIndexN - 1;
+                        charIndex = charIndexN - 1; //esto es xq al no encontrarse con un numero a diferencia del string con el "", se pierde ese char si no se le deja el -1
                         break;
                     }
                 }
@@ -85,49 +86,84 @@ public class Lexer {
 //                }
         //    }
             else if (currentWord.equals("let")) {
-                tokens.add(new Token("variableDeclaration", currentWord, row, column));
+                tokens.add(new Token("variableDeclaration", currentWord, row, column - currentWord.length()));
                 currentWord = "";
-            } // verificar si es un + - * /
+            }
+
+            // verificar si es un + - * /
             else if ((currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/') ) {
+                column++;
+                // Si la currentWord no esta vacia es porque es una nameVariable
+                // (si fuera un string-number ya hubiese sido agregado)
                 if (!currentWord.isEmpty()) {
                     identifier = "nameVariable";
-                    tokens.add(new Token(identifier, currentWord, row, column));
+                    tokens.add(new Token(identifier, currentWord, row, column - currentWord.length() + spaceCounter));
                     currentWord = "";
                 }
-
                 identifier = "operator";
                 currentWord += currentChar;
-                tokens.add(new Token(identifier, currentWord, row, column));
+                tokens.add(new Token(identifier, currentWord, row, column ));
                 currentWord = "";
-            } else if (currentChar == ':') {
-                tokens.add(new Token("nameVariable", currentWord, row, column));
-                tokens.add(new Token("dataTypeAssignation", ":", row, column));
-                currentWord = "";
-            }else if (currentChar == '=') {
-                tokens.add(new Token("symbol", currentWord, row, column));
-                tokens.add(new Token("valueAssignation", "=", row, column));
-                currentWord = "";
-            }  else if (currentChar == ';') {
+            }
+
+            else if (currentChar == ':') {
+                column++;
+                // Si la currentWord no esta vacia es porque es una nameVariable
+                // (si fuera un string-number ya hubiese sido agregado)
                 if (!currentWord.isEmpty()) {
                     identifier = "nameVariable";
-                    tokens.add(new Token(identifier, currentWord, row, column));
+                    tokens.add(new Token(identifier, currentWord, row, column - currentWord.length() - spaceCounter));
                 }
-                tokens.add(new Token("endLine", ";", row, column));
+
+                tokens.add(new Token("dataTypeAssignation", ":", row, column));
                 currentWord = "";
-                row++;
-            } else if (currentChar == '\n') {
-                row++;
+            }
+
+            else if (currentChar == '=') {
+                // Si la currentWord no esta vacia es porque es una nameVariable
+                // (si fuera un string-number ya hubiese sido agregado)
+                if (!currentWord.isEmpty()) {
+                    identifier = "symbol";
+                    tokens.add(new Token(identifier, currentWord, row, column - currentWord.length()));
+                }
                 column++;
-            } else if (currentChar == '\t') {
+
+                tokens.add(new Token("valueAssignation", "=", row, column ));
+                currentWord = "";
+            }
+
+            else if (currentChar == ';') {
+                column++;
+                // Si la currentWord no esta vacia es porque es una nameVariable
+                // (si fuera un string-number ya hubiese sido agregado)
+                if (!currentWord.isEmpty()) {
+                    identifier = "nameVariable";
+                    tokens.add(new Token(identifier, currentWord, row, column - currentWord.length() + spaceCounter));
+                }
+
+                tokens.add(new Token("endLine", ";", row, column ));
+                currentWord = "";
+                spaceCounter = 0;
+            }
+
+            else if (currentChar == '\n') {
+                row++;
+                column = 1;
+            }
+
+            else if (currentChar == '\t') {
                 column += 4;
-            } else if (currentChar == ' ') {
-                if (currentWord.isEmpty()) column++;
-                //tokens.add(new Token(identifier, currentWord, row, column));
+            }
+
+            else if (currentChar == ' ') {
+                column++;
+                spaceCounter++;
             }
 
             // agrega el caracter actual a la palabra actual si no es nada (seguir rellenando)
             else {
                 currentWord += currentChar;
+                column++;
             }
         }
         // Verifica si la palabra actual no esta vacia y la agrega a la lista de tokens SI no se cerro un string ocn comillas
@@ -141,10 +177,11 @@ public class Lexer {
 
     public static void main(String[] args) {
         Lexer lexer = new Lexer();
-        List<Token> tokens = lexer.lex("let a: number = 12; let b: number = 4; let c: number = a / b;");
+        List<Token> tokens = lexer.lex("let a : number = 12;\nlet b: number = 4;\nlet c: number = a / b;");
         for (Token token : tokens) {
             System.out.println(token);
         }
 
     }
 }
+// TODO (solo para resaltar) la columna es ANTES del char
